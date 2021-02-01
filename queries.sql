bd66b5ae5cfe1518f154c82d73bfd930e0855e2a
Maksim Koshman <maksimkoshman090@gmail.com>
2021-02-01 10:24:15 +0300

    Добавил SQL запросы

---
+++ queries.sql
@@ -0,0 +1,73 @@
+# Заполнение категорий
+INSERT INTO categories SET title = 'Доски и лыжи';
+INSERT INTO categories SET title = 'Крепления';
+INSERT INTO categories SET title = 'Ботинки';
+INSERT INTO categories SET title = 'Одежда';
+INSERT INTO categories SET title = 'Инструменты';
+INSERT INTO categories SET title = 'Разное';
+
+# Заполнение пользователей
+INSERT INTO users SET name = 'Игнат', email = 'ignat.v@gmail.com', password = '$2y$10$OqvsKHQwr0Wk6FMZDoHo1uHoXd4UdxJG/5UDtUiie00XaxMHrW8ka';
+INSERT INTO users SET name = 'Леночка', email = 'kitty_93@li.ru', password = '$2y$10$bWtSjUhwgggtxrnJ7rxmIe63ABubHQs0AS0hgnOo41IEdMHkYoSVa';
+INSERT INTO users SET name = 'Руслан', email = 'warrior07@mail.ru', password = '$2y$10$2OxpEH7narYpkOT1H5cApezuzh10tZEEQ2axgFOaKW.55LxIJBgWW';
+
+# Заполнение объявлений
+INSERT INTO ads SET
+                    title = '014 Rossignol District Snowboard',
+                    user_id = 3,
+                    category_id = 1,
+                    price = 10999,
+                    price_start = 10999,
+                    rates_count = 20,
+                    img = 'img/lot-1.jpg',
+                    is_active = 1;
+INSERT INTO ads SET
+                    title = 'DC Ply Mens 2016/2017 Snowboard',
+                    user_id = 2,
+                    category_id = 1,
+                    price = 15999,
+                    price_start = 15999,
+                    rates_count = 11,
+                    img = 'img/lot-2.jpg',
+                    is_active = 1;
+INSERT INTO ads SET
+                    title = 'Крепления Union Contact Pro 2015 года размер L/XL',
+                    user_id = 1,
+                    category_id = 2,
+                    price = 8000,
+                    price_start = 8000,
+                    rates_count = 5,
+                    img = 'img/lot-3.jpg',
+                    is_active = 1;
+INSERT INTO ads SET
+                    title = 'Ботинки для сноуборда DC Mutiny Charocal',
+                    user_id = 2,
+                    category_id = 3,
+                    price = 10999,
+                    price_start = 10999,
+                    rates_count = 0,
+                    img = 'img/lot-4.jpg',
+                    is_active = 1;
+INSERT INTO ads SET
+                    title = 'Куртка для сноуборда DC Mutiny Charocal',
+                    user_id = 1,
+                    category_id = 4,
+                    price = 10999,
+                    price_start = 10999,
+                    rates_count = 1,
+                    img = 'img/lot-5.jpg',
+                    is_active = 1;
+INSERT INTO ads SET
+                    title = 'Маска Oakley Canopy',
+                    user_id = 3,
+                    category_id = 6,
+                    price = 5500,
+                    price_start = 5500,
+                    rates_count = 4,
+                    img = 'img/lot-6.jpg',
+                    is_active = 1;
+
+# Заполнение ставок
+INSERT INTO rates SET user_id = 1, ad_id = 1, price = 12000, rates_date = NOW();
+INSERT INTO rates SET user_id = 2, ad_id = 5, price = 7000, rates_date = NOW();
+INSERT INTO rates SET user_id = 3, ad_id = 3, price = 10500, rates_date = NOW();
---
+++ schema.sql
@@ -0,0 +1,45 @@
+CREATE TABLE categories (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    title CHAR(128)
+);
+
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name CHAR(128),
+    email CHAR (255),
+    password CHAR(64)
+);
+
+CREATE UNIQUE INDEX x_title ON categories(title);
+CREATE UNIQUE INDEX x_email ON users(email);
+
+CREATE TABLE ads (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT,
+    title CHAR(128),
+    category_id INT,
+    price INT,
+    price_start INT,
+    rates_count INT,
+    img CHAR(255),
+    is_active INT,
+    CONSTRAINT ads_user_id FOREIGN KEY (user_id)  REFERENCES users(id),
+    CONSTRAINT ads_category FOREIGN KEY (category_id) REFERENCES categories(id)
+);
+
+CREATE UNIQUE INDEX x_title ON ads(title);
+CREATE INDEX x_name ON users(name);
+CREATE INDEX x_price ON ads(price);
+
+CREATE TABLE rates (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT,
+    ad_id INT,
+    price INT,
+    rates_date DATETIME  NOT NULL,
+    CONSTRAINT rates_user_id FOREIGN KEY (user_id)  REFERENCES users(id),
+    CONSTRAINT rates_ad_title FOREIGN KEY (ad_id)  REFERENCES ads(id)
+);
+
+
+
