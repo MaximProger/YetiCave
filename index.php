ab16279224131d9d20557fe09596300c19299308
Maksim Koshman <maksimkoshman090@gmail.com>
2021-02-02 14:19:32 +0300

    Добавил выод лотов на главной из БД

--- index.php
+++ index.php
@@ -2,7 +2,26 @@
 
 require_once 'data.php';
 require_once 'functions.php';
-require_once 'userdata.php';
+require_once 'init.php';
+
+// Получаем массив пользователей
+$ads = [];
+
+if (!$link) {
+    $error = mysqli_connect_error();
+    $content = renderTemplate('templates/error.php', ['error' => $error]);
+} else {
+    $sql = 'SELECT c.title as category, a.title, a.price, a.img FROM ads a
+JOIN categories c ON c.id = a.category_id';
+    $result = mysqli_query($link, $sql);
+
+    if ($result) {
+        $ads = mysqli_fetch_all($result, MYSQLI_ASSOC);
+    } else {
+        $error = mysqli_error($link);
+        $content = renderTemplate('templates/error.php', ['error' => $error]);
+    }
+}
 
 $page_content = renderTemplate('templates/index.php', ['ads' => $ads, 'date' => $untilMidnightDate]);
 
